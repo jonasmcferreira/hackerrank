0918876312d59beea314ecb912b686af527fdff9
jonas.mcferreira <jonas.mcferreira@gmail.com>
2016-11-18 11:55:22 +0000

    fixed circular array rotation

--- Algorithms/CircularArrayRotation.java
+++ Algorithms/CircularArrayRotation.java
@@ -8,15 +8,19 @@ public class CircularArrayRotation {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        
         int n = in.nextInt();
         int k = in.nextInt();
         int q = in.nextInt();
         int[] a = new int[n];
+        
         for(int a_i=0; a_i < n; a_i++){
-            a[a_i] = in.nextInt();
+            a[(a_i + k)%n] = in.nextInt();
         }
+
         for(int a0 = 0; a0 < q; a0++){
             int m = in.nextInt();
+            System.out.println(a[m]);
         }
     }
 }
