d5998d615624e814efae50d8f9de0ff6891836b4
jonas.mcferreira <jonas.mcferreira@gmail.com>
2016-11-18 15:30:06 +0000

    gitwatch autoCommit 20161118.153006

---
+++ Strings/Anagrams.java
@@ -0,0 +1,30 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Anagrams {
+
+	static boolean isAnagram(String a, String b) {
+
+		char[] ar = a.toLowerCase().toCharArray();
+		char[] br = b.toLowerCase().toCharArray();
+
+		Arrays.sort(ar);
+		Arrays.sort(br);
+
+		return Arrays.equals(ar, br);
+
+
+	}
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		String a = scan.next();
+		String b = scan.next();
+		scan.close();
+		
+		boolean ret = isAnagram(a, b);
+		System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+	}
+}
---
+++ Strings/StringTokens.java
@@ -0,0 +1,36 @@
+
+import java.io.*;
+import java.util.*;
+
+public class StringTokens {
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine().trim();
+        
+        if(s.length() == 0){
+        
+          System.out.println("0");  
+        
+        } else if (s.length() > 400000){
+             
+        } else {
+        
+            String[] result = s.split("[ !,?._'@]+");
+        
+            System.out.println(result.length);
+        
+            for (int x=0; x<result.length; x++) {
+        
+              System.out.println(result[x]);
+        
+            }    
+        
+        }
+        
+        
+        
+        scan.close();
+    }
+}
